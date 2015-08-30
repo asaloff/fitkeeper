a854cda73c45895763d6c3e5fafbb9cfa843756f
Aaron Saloff <aaronsaloff@gmail.com>
2015-08-30 16:08:16 -0400

    capitalized attributes

--- spec/models/exercise_spec.rb
+++ spec/models/exercise_spec.rb
@@ -38,33 +38,33 @@ describe Exercise do
     let(:sarah) { Fabricate(:user) }
 
     it "returns the excercise's current paramaters" do
-      exercise = Exercise.create(name: 'Leg Day', user: sarah, weight: 10, time: 30, time_type: 'seconds' ,sets: 5, reps: 10) 
-      expect(exercise.display_current_exercise_status).to eq '10 lbs, 30 seconds, 5 sets, 10 reps'
+      exercise = Exercise.create(name: 'Leg Day', user: sarah, weight: 10, time: 30, time_type: 'Seconds' ,sets: 5, reps: 10) 
+      expect(exercise.display_current_exercise_status).to eq '10 LBS, 30 Seconds, 5 Sets, 10 Reps'
     end
 
     it 'returns only two paramaters if the excercise has only two' do
       exercise = Exercise.create(name: 'Leg Day', user: sarah, weight: 10, reps: 10) 
-      expect(exercise.display_current_exercise_status).to eq '10 lbs, 10 reps'
+      expect(exercise.display_current_exercise_status).to eq '10 LBS, 10 Reps'
     end
 
     it 'returns only three paramaters if the excercise has only three' do
       exercise = Exercise.create(name: 'Leg Day', user: sarah, weight: 10, sets: 5, reps: 10) 
-      expect(exercise.display_current_exercise_status).to eq '10 lbs, 5 sets, 10 reps'
+      expect(exercise.display_current_exercise_status).to eq '10 LBS, 5 Sets, 10 Reps'
     end
 
     it 'does not return the time type if there is no time amount' do
       exercise = Exercise.create(name: 'Leg Day', user: sarah, weight: 10, sets: 5, reps: 10) 
-      expect(exercise.display_current_exercise_status).to eq '10 lbs, 5 sets, 10 reps' 
+      expect(exercise.display_current_exercise_status).to eq '10 LBS, 5 Sets, 10 Reps' 
     end
 
     it 'does not have a comma at the front of the string' do
        exercise = Exercise.create(name: 'Leg Day', user: sarah, sets: 5, reps: 10) 
-      expect(exercise.display_current_exercise_status).to eq '5 sets, 10 reps' 
+      expect(exercise.display_current_exercise_status).to eq '5 Sets, 10 Reps' 
     end
 
     it 'does not have a comma at the end of the string' do
-      exercise = Exercise.create(name: 'Leg Day', user: sarah, weight: 10, time: 30, time_type: 'seconds' , sets: 5) 
-      expect(exercise.display_current_exercise_status).to eq '10 lbs, 30 seconds, 5 sets'
+      exercise = Exercise.create(name: 'Leg Day', user: sarah, weight: 10, time: 30, time_type: 'Seconds' , sets: 5) 
+      expect(exercise.display_current_exercise_status).to eq '10 LBS, 30 Seconds, 5 Sets'
     end
   end
 end
